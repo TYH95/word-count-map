bdfcf90d9adb5a4b6ee31f5cb2f744bcddee5434
Till Hahn <till@plakos.de>
2023-08-19 11:18:06 +0200

    adds actorsystem to transform string into simple wordcountmap

--- wcm-backend/build.sbt
+++ wcm-backend/build.sbt
@@ -1,12 +1,21 @@
 val scala3Version = "3.3.0"
 
+val AkkaVersion = "2.7.0"
+val AkkaHttpVersion = "10.5.2"
+
 lazy val root = project
   .in(file("."))
   .settings(
     name := "wcm-backend",
     version := "0.1.0-SNAPSHOT",
-
     scalaVersion := scala3Version,
-
-    libraryDependencies += "org.scalameta" %% "munit" % "0.7.29" % Test
+    resolvers += "Akka library repository".at("https://repo.akka.io/maven"),
+    libraryDependencies ++= Seq(
+      "com.typesafe.akka" %% "akka-actor-typed" % AkkaVersion,
+      "com.typesafe.akka" %% "akka-actor-testkit-typed" % AkkaVersion % Test,
+      "com.typesafe.akka" %% "akka-stream" % AkkaVersion,
+      "com.typesafe.akka" %% "akka-stream-testkit" % AkkaVersion,
+      "com.typesafe.akka" %% "akka-http" % AkkaHttpVersion,
+      "com.typesafe.akka" %% "akka-http-testkit" % AkkaHttpVersion
+    )
   )
--- wcm-backend/src/main/scala/Main.scala
+++ wcm-backend/src/main/scala/Main.scala
@@ -1,5 +1,51 @@
-@main def hello: Unit =
-  println("Hello world!")
-  println(msg)
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.Behavior
+import akka.actor.typed.scaladsl.AbstractBehavior
+import akka.actor.typed.scaladsl.ActorContext
+import akka.actor.typed.scaladsl.Behaviors
+import scala.collection.MapView
 
-def msg = "I was compiled by Scala 3. :)"
+object StringToWordcountMapActor {
+
+  final case class TransformToMap(value: String)
+
+  def apply(): Behavior[TransformToMap] =
+    Behaviors.setup { context =>
+      Behaviors.receiveMessage { message => {
+        message match {
+          case TransformToMap(value) => 
+            val wordCountMap: MapView[String, Integer] = value.split("\\s+").groupBy(w => w).mapValues(_.size)
+            println(wordCountMap.view.mapValues((x) => x).toMap)
+            Behaviors.same
+        }
+      }}
+    }
+
+}
+
+object GatherPostActor {
+
+  final case class GatherPost(postContent: String)
+
+  def apply(): Behavior[GatherPost] =
+    Behaviors.setup { context =>
+      val transformer =
+        context.spawn(StringToWordcountMapActor(), "transformer")
+
+      Behaviors.receiveMessage { message =>
+        message match {
+          case GatherPost(postContent) =>
+            transformer ! StringToWordcountMapActor.TransformToMap(postContent)
+            Behaviors.same
+        }
+      }
+    }
+
+}
+
+object WordCountMap extends App {
+  val system: ActorSystem[GatherPostActor.GatherPost] =
+    ActorSystem(GatherPostActor(), "gather")
+
+  system ! GatherPostActor.GatherPost("Ein einfaches Beispiel mit einem Beispiel doppelt.")
+}
