bb6f187e75663e08bb442ed851bc02b37288fdd5
Till Hahn <till@plakos.de>
2023-08-20 20:19:48 +0200

    configures meta values

--- wcm-frontend/src/app/App.js
+++ wcm-frontend/src/app/App.js
@@ -2,8 +2,10 @@ import "./App.css";
 import useWebSocket, { STATUSES } from "@components/useWebsocket";
 import WordCountMapViewer from "@components/WordCountMapViewer";
 import styled from "@emotion/styled";
-import { Button, CircularProgress, Paper } from "@mui/material";
+import { Button, CircularProgress } from "@mui/material";
 import { useEffect, useState } from "react";
+
+const WEBSOCKET_ENDPOINT = 'ws:127.0.0.1:8080/ws'
 
 const Heading = styled('h1')({
   color: 'darkslategray',
@@ -17,10 +19,9 @@ const ContentWrapper = styled('div')({
   padding: 8,
 });
 
-
 function App() {
   const [retry, setRetry] = useState(false)
-  const connectionStatus = useWebSocket('ws:127.0.0.1:8080/ws', retry)
+  const connectionStatus = useWebSocket(WEBSOCKET_ENDPOINT, retry)
 
   useEffect(() => {
     if (connectionStatus === STATUSES.CLOSED) {
